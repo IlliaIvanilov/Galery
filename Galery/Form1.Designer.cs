de0b5bf5d1ac2044be6d206bc942c56fde9be8fa
Микола Дробница <nikolaidrobnitsa@gmail.com>
2022-02-11 17:51:09 +0200

    end logic

--- Galery/Form1.Designer.cs
+++ Galery/Form1.Designer.cs
@@ -1,17 +1,22 @@
-﻿
-namespace Galery
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using System.Linq;
+using System.Globalization;
+namespace Galary
 {
     partial class Form1
     {
         /// <summary>
-        ///  Required designer variable.
+        /// Обязательная переменная конструктора.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        ///  Clean up any resources being used.
+        /// Освободить все используемые ресурсы.
         /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -21,19 +26,192 @@ namespace Galery
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
+        #region Код, автоматически созданный конструктором форм Windows
 
         /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
         /// </summary>
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1500, 900);
+            this.Text = "Gallery";
+            this.BackColor = System.Drawing.Color.LightCyan;
+            if (File.Exists("images.txt"))
+                PathToFile.AddRange(File.ReadAllLines("images.txt"));
+
+            //label.Text = string.Format("{0}/{1}", num, count);
+            //label.Multiline = true;
+            label.Font = new System.Drawing.Font("Time New Roman", 50, System.Drawing.FontStyle.Bold);
+            label.Location = new System.Drawing.Point(700, 10);
+            label.Size = new System.Drawing.Size(130, 80);
+            this.Controls.Add(label);
+
+
+
+            //saveBtn.Click += SaveBtn_Click;
+            //saveBtn.Text = "Save";
+            //saveBtn.Location = new System.Drawing.Point(150, 50);
+            //this.Controls.Add(saveBtn);
+
+            dowloadBtn.Click += DowloadBtn_Click;
+            dowloadBtn.Text = "+";
+            dowloadBtn.Font = new System.Drawing.Font("Time New Roman", 40, System.Drawing.FontStyle.Bold);
+            dowloadBtn.Location = new System.Drawing.Point(1400, 20);
+            dowloadBtn.Size = new System.Drawing.Size(60, 60);
+            dowloadBtn.BackColor = System.Drawing.Color.LightBlue;
+            this.Controls.Add(dowloadBtn);
+
+
+            LeftBtn.Click += LeftBtn_Click;
+            LeftBtn.Text = "<";
+            LeftBtn.Location = new System.Drawing.Point(0, 400);
+            LeftBtn.Font = new System.Drawing.Font("Time New Roman", 50, System.Drawing.FontStyle.Bold);
+            LeftBtn.Size = new System.Drawing.Size(100, 100);
+            LeftBtn.BackColor = System.Drawing.Color.LightGray;
+            LeftBtn.Enabled = false;
+            this.Controls.Add(LeftBtn);
+
+
+            RightBtn.Click += RightBtn_Click;
+            RightBtn.Text = ">";
+            RightBtn.Location = new System.Drawing.Point(1400, 400);
+            RightBtn.Font = new System.Drawing.Font("Time New Roman", 50, System.Drawing.FontStyle.Bold);
+            RightBtn.Size = new System.Drawing.Size(100, 100);
+            RightBtn.BackColor = System.Drawing.Color.LightGray;
+            this.Controls.Add(RightBtn);
+            this.FormClosing += Form1_FormClosing;
+
+            delete.Click += DeleteBtn_Click;
+            delete.Text = "-";
+            delete.Location = new System.Drawing.Point(1400, 520);
+            delete.Font = new System.Drawing.Font("Time New Roman", 50, System.Drawing.FontStyle.Bold);
+            delete.Size = new System.Drawing.Size(100, 100);
+            delete.BackColor = System.Drawing.Color.LightGray;
+            this.Controls.Add(delete);
+
+            picture.Location = new System.Drawing.Point(250, 100);
+            picture.Size = new System.Drawing.Size(1000, 1000);
+            picture.SizeMode = PictureBoxSizeMode.StretchImage;
+            this.Controls.Add(picture);
+            SetImage(num);
+            //int l = Int32.Parse(last_num);
+
         }
+        
+        static int num = 0;
+        string last_num = File.ReadAllText("last_photo.txt");
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            File.WriteAllLines("images.txt", PathToFile);
+            File.WriteAllText("last_photo.txt", num.ToString());
+            //foreach (var s in "images.txt")
+            //{
+            //    numPath++;
+            //    c++;
+            //}
+            //if(numPath != PathToFile.Count)
+            //{
+            //    File.Delete("images.txt");
+            //}
+        }
+        private void DeleteBtn_Click(object sender, System.EventArgs e)
+        {
+            if(num >= 1)
+            {
+                PathToFile.Remove(PathToFile[num]);
+            }
+            else
+            {
+                label.Text += " ";
+            }
+        }
+
+        private void RightBtn_Click(object sender, System.EventArgs e)
+        {
+            LeftBtn.Enabled = true;
+            if (PathToFile.Count == num + 2)
+            {
+                RightBtn.Enabled = false;
+            }
+            num++;
+            SetImage(num);
+            if (PathToFile.Count == 1)
+            {
+                RightBtn.Enabled = false;
+            }
+        }
+
+        private void LeftBtn_Click(object sender, System.EventArgs e)
+        {
+            
+            RightBtn.Enabled = true;
+            num--;
+            if (num == 0)
+            {
+                LeftBtn.Enabled = false;
+            }
+            else
+            {
+                LeftBtn.Enabled = true;
+            }
+            SetImage(num);
+            if (PathToFile.Count == 1)
+            {
+                LeftBtn.Enabled = false;
+            }
+
+        }
+        private void DowloadBtn_Click(object sender, System.EventArgs e)
+        {
+            using (OpenFileDialog dialog = new OpenFileDialog())
+            {
+                dialog.Filter = "*.jpg|*.jpg|*.png|*.png";
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    PathToFile.Add(dialog.FileName);
+                    
+                }
+            }
+        }
+        void SetImage(int indx)
+        {
+            label.Text = $"{num + 1}/{PathToFile.Count.ToString()}";
+            if(PathToFile.Count == 1)
+            {
+                LeftBtn.Enabled=false;
+                RightBtn.Enabled=false;
+                delete.Enabled=false;
+            }
+            if (PathToFile.Count > 1)
+            {
+                LeftBtn.Enabled = true;
+                RightBtn.Enabled = true;
+                delete.Enabled = true;
+            }
+            try
+            {
+               
+                picture.Image = System.Drawing.Image.FromFile(PathToFile[num]);
+            }
+            catch 
+            {
+                label.Text = " ";
+            }
+        }
+
+        Button LeftBtn = new Button();
+        Button RightBtn = new Button();
+        Button saveBtn = new Button();
+        Button dowloadBtn = new Button();
+
+        List<string> PathToFile = new List<string>();
+        Label label = new Label();
+        Label labels = new Label();
+        PictureBox picture = new PictureBox();
+        Button delete = new Button();
 
         #endregion
     }
